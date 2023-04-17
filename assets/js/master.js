9da18244a89f6ca429493cc8e5132b2ec33abf21
abbasebadian <abbasebadiann@gmail.com>
2023-04-17 20:25:33 +0330

    meeting interactivity

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,16 +1,62 @@
-document.onreadystatechange = function () {
-    
-    initSelectBoxEventListener();
+var init = false
+var INVISIBLE = {
+    opacity: 0,
+    pointerEvents: "none",
+    visibility: "hidden" 
 }
 
-const initSelectBoxEventListener = () => {
-    const $selectBox = $(".selectBox");
-    console.log($selectBox)
-    $selectBox.children("button").click(function(e) {
-        const $parent = $(e.target).parents("button").length > 0 ? $(e.target).parents("button"): $(e.target);
-        console.log($parent.siblings("ul"))
-        $parent.siblings("ul").toggleClass('hidden')
-        
-    })
+var VISIBLE = {
+    opacity: 1,
+    pointerEvents: "all",
+    visibility: "visible" 
+}
+
+document.onreadystatechange = function () {
+    if (init) return
+    init = true;
+
+    initTogglers();
+
+    initMeetingReminder();
+}
+
+const initMeetingReminder = () => {
+    const meetingReminder = $('[data-meeting-reminder]')
     
+    if(!meetingReminder.length) return
+
+    setTimeout(() => {
+        if( !meetingReminder.data("should-show")) return
+        meetingReminder.css(VISIBLE);
+    }, 
+    2000)
+
+    meetingReminder.find("[closer]").on('click', () => {
+        meetingReminder.css(INVISIBLE)
+    })
+}
+
+const initTogglers = () => {
+    
+    $("[dropdown-toggler]").each((_, item) => {
+        $(item).next("[togglable]").css(INVISIBLE)
+        $(item).on('click', (event) => {
+            const visible = $(item).next("[togglable]").css('visibility')
+            if (visible === 'visible') {
+                $(item).next("[togglable]").css(INVISIBLE);
+            } else {
+                $(item).next("[togglable]").css(VISIBLE);
+            }
+        })
+    })
+    $('[closer]').on('click', (e) => {
+        $(e.target).closest("[togglable]").css(INVISIBLE)
+    })
+
+    $("[modal-toggler]").each((_, item) => {
+        $(item).on('click', () => {
+            $($(item).data('target')).css(VISIBLE)
+        })
+    })
+
 }
